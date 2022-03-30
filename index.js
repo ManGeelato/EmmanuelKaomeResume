43e52a5dc1e79eea082b6b864e9727b16ea2d21f
ManGeelato <92035453+ManGeelato@users.noreply.github.com>
2022-03-30 11:22:17 +0200

    My resume website

---
+++ index.js
@@ -0,0 +1,16 @@
+// function to alert client details on submit
+// I referenced some of the below knowledge from my previous tasks
+function onClickGit(){
+    alert('You are now being redirected to Github page!')
+}
+
+function onClickLinkedIn(){
+    alert('You are now being redirected to LinkedIn page!')
+}
+
+function onClickResume(){
+    alert('You are now being redirected to the download page!')
+}
+function addClient(){
+   alert('Thank You for reaching out!')
+}
